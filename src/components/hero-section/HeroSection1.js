06c42ba157328ae7aed561894f5d6d7f3c3ae0cd
Santo Khan <santokhan1999@gmail.com>
2023-01-25 04:56:24 +0600

    remove effect from slider

--- src/components/hero-section/HeroSection1.js
+++ src/components/hero-section/HeroSection1.js
@@ -4,7 +4,7 @@ import Title from "./Title";
 export default function HeroSection1({ startScroll }) {
   return (
     <div className="slide-2 flex justify-center items-center h-full w-full">
-      <div className="effect-1 w-full h-full flex justify-center items-center">
+      <div className="effec w-full h-full flex justify-center items-center">
         <div className="text-start lg:text-center max-w-7xl p-4">
           <Title>Registered Massage Therapists</Title>
         </div>
