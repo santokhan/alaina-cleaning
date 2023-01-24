7da2cdc97a467b97ec7cae09630b9175b234f86c
Santo Khan <santokhan1999@gmail.com>
2023-01-24 23:54:19 +0600

    initial commit

--- src/components/hero-section/HeroSection2.js
+++ src/components/hero-section/HeroSection2.js
@@ -6,7 +6,7 @@ export default function HeroSection2({ startScroll }) {
     <div className="slide-3 flex justify-center items-center h-full w-full">
       <div className="effect-1 w-full h-full flex justify-center items-center">
         <div className="text-start lg:text-center max-w-7xl p-4">
-          <Title>Massage Jan Morks</Title>
+          <Title>We are care aboout our customers</Title>
         </div>
       </div>
     </div>
