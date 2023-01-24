3218a6371d539d2bf90c8e40a3bb92a3b7210f1b
Santo Khan <santokhan1999@gmail.com>
2023-01-25 04:52:04 +0600

    remove slider dark effect

--- src/components/hero-section/HeroSection.js
+++ src/components/hero-section/HeroSection.js
@@ -4,7 +4,7 @@ import Title from "./Title";
 export default function HeroSection({ startScroll }) {
   return (
     <div className="slide-1 flex justify-center items-center h-full w-full">
-      <div className="effect w-full h-full flex justify-center items-center">
+      <div className="effec w-full h-full flex justify-center items-center">
         <div className="text-center max-w-7xl p-4">
           <Title>Massage Jan Morks</Title>
         </div>
