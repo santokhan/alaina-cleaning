b79c1a1d07e3c6dbb48110029aa32582f5a93e10
Santo Khan <santokhan1999@gmail.com>
2023-01-28 18:44:56 +0600

    Logo size

--- src/components/header/logo/Logo.js
+++ src/components/header/logo/Logo.js
@@ -4,7 +4,7 @@ import logo from "./logo.png";
 export default function Logo() {
   return (
     <Link to="/">
-      <img src={logo} alt="logo" className="h-12 lg:h-16 object-contain logo" />
+      <img src={logo} alt="logo" className="h-12 lg:h-14 object-contain logo" />
     </Link>
   );
 }
